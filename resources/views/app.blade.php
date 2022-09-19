f6d40cee9e1358669fb3437bcf64b460c4b25805
= <=>
2022-09-19 11:08:15 +0500

    stylesheer

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Scripts -->
         @routes
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link  href="{{ mix('css/app.css') }}">
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
